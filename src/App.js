f7bdec1bbbeafa21515fdded43535570cad0afbb
Simon Mai <maismin@gmail.com>
2021-04-08 20:29:39 -0700

    Change app text

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          HELLO World!
+          I was changed on the feature branch
         </p>
         <a
           className="App-link"
